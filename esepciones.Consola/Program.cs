97d11d742fce3f8edae92fd72f95d6c0021784ad
alexanderdeoz <sad.chinchin@yavirac.edu.ec>
2020-12-27 00:07:39 -0500

    esepciones try

---
+++ esepciones.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace esepciones.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio, total;
+            int cantidad;
+           
+            // try catch
+
+            try
+            {
+                Console.WriteLine("Consulta de esepciones ");
+                Console.WriteLine("Ejemplo simple de try catch");
+                Console.WriteLine("Cantidad : ");
+                cantidad = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Precio : ");
+                precio = Convert.ToDouble(Console.ReadLine());
+                total = cantidad * precio;
+                Console.WriteLine("el total es  : " + total);
+            } catch (Exception error)
+            {
+                Console.WriteLine("ha ocurrido un error "+error.Message);
+            
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
